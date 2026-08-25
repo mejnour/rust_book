2bc83cd9bc8e26886a54a77ef751fb0a392f00de
Arnaldo Pina Neto <apina.dev@gmail.com>
2026-08-24 22:08:27 -0300

    references and borrowing update

--- ownership/src/bin/ref_n_borrow.rs
+++ ownership/src/bin/ref_n_borrow.rs
@@ -4,6 +4,12 @@ fn calculare_length(s: &String) -> usize {
 
 fn change(s3: &String) {
     // s3.push_str(", la"); // não funciona pq a ref da linha 5 não é mut
+    println!("s3: {}", s3);
+}
+
+fn mutable_change(s4: &mut String) {
+    s4.push_str(" la");
+    println!("s4: {}", s4);
 }
 
 fn main() {
@@ -14,4 +20,20 @@ fn main() {
     println!("str {s1} w len {len}");
 
     change(&s1);
+
+    let mut s1 = String::from("hello");
+
+    mutable_change(&mut s1);
+    mutable_change(&mut s1); // o escopo de mutable_change() acabou e é possível reutilizar
+    {
+        let _s2 = &s1; // também tem escopo definido
+    }
+
+    let _s3 = &mut s1;
+    let _s4 = &mut s1;
+    // println!("{_s3}, {_s4}"); // só pode fazer borrow de mut ref de s1 1x
+
+    let _s5 = &s1; // ref fixa pra s1
+    let _s6 = &mut s1; // ref mut pra s1
+    // println!("{_s5}, {_s6}"); // dá ruim
 }
