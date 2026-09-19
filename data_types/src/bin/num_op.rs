6e1a526fbe477ca60f6ca1ce5f634c5bf25c1ae9
Arnaldo Pina Neto <apina.dev@gmail.com>
2026-09-19 17:43:40 -0300

    basic operations

--- data_types/src/bin/num_op.rs
+++ data_types/src/bin/num_op.rs
@@ -20,4 +20,16 @@ fn main() {
     // remainder
     let remainder = 43 % 5;
     println!("remainder: {remainder}");
+
+    // power
+    let power_of = 2_i32.pow(2);
+    println!("potenciacao int {}", power_of);
+
+    // power float
+    let power_of = 2.0_f32.powf(2.0);
+    println!("potenciacao float {}", power_of);
+
+    // raiz quadrada
+    let sqrt_ex = 16_f32.sqrt(); // só existe sqrt() para tipos float, f32 e f64
+    println!("raiz quadrada de 16.0 é {}", sqrt_ex);
 }
